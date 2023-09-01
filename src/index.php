62aee98b25aca0373762a06394a0a3da2ddbc70f
Guilherme Andrade <42524262+guiandradedev@users.noreply.github.com>
2023-09-01 01:23:23 -0300

    :truck: chore: change index.html to index.php

---
+++ src/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Banco de dados</title>
+</head>
+<body>
+    <div class="center-box">
+        <input type="text" class="input1" placeholder="Itens">
+        <input type="text" class="input2" placeholder="N°">
+        <button type="submit" class="styled-button">Enviar</button><br><br\>
+      </div>
+        
+	
+	<div class="table">
+		<div class="table-header">
+			<div class="header__item"><a id="name" class="filter__link" href="#">Item</a></div>
+			<div class="header__item"><a id="wins" class="filter__link filter__link--number" href="#">Quantidade</a></div>
+		</div>
+		<div class="table-content">	
+			<div class="table-row">		
+				<div class="table-data">Pacote de arroz</div>
+				<div class="table-data">2</div>
+			</div>
+			<div class="table-row">
+				<div class="table-data">Caixa de Leite</div>
+				<div class="table-data">1</div>
+			</div>
+			<div class="table-row">
+				<div class="table-data">Garrafa de água</div>
+				<div class="table-data">4</div>
+			</div>
+		</div>	
+	</div>
+</body>
+</html>
